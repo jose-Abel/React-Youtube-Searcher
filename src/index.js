dfd35949ee205f213f14293f2c6a1de9263395b4
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-12 13:39:27 -0400

    Update the search_bar class render method to create a controlled component for the input by giving its value through the state of the component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import SearchBar from "./components/search_bar";
 
-
 // Create a new component. This component should produce some HTML
 
 const App = () => {
